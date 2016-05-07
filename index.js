20c9c758bbd86721022d607870d2534c2491e52c
zyam <thepredestrian@gmail.com>
2016-05-07 18:44:36 -0400

    timer

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook/', function (req, res) {
 
             else if (text === 'remindme') {
               sendTextMessage(sender, "Countdown started!");
-              startCountdown(sender);
+              setTimeout(sendTextMessage(sender, "Time's up!"), 3000);
               continue;
 
             }
@@ -61,9 +61,6 @@ app.post('/webhook/', function (req, res) {
 
 var token = "EAACf3gk27BABAF5HaqD36bv3wfh6pxHTFAJFfxKE00L4PpcuAy4BohC47WolMnk2IlUDyb1HnmBzmXzO965BBeg2rD1ZB570o2HcnWIxZBXxzkIJzwdmFZB3axLDSVBZCQRbZBzX9xMvi5TrzHkdSashtPlHZBKJ9kOHPw0SBx7wZDZD"
 
-function startCountdown(sender) {
-  setTimeout(sendTextMessage(sender, "Time's up!"), 3000);
-}
 
 function sendTextMessage(sender, text) {
     messageData = {
