f825fc3e20d2051b92f7e8d69b8e4926c780c5e3
zyam <thepredestrian@gmail.com>
2016-05-07 15:52:38 -0400

    test

--- index.js
+++ index.js
@@ -28,3 +28,39 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAACf3gk27BABAF5HaqD36bv3wfh6pxHTFAJFfxKE00L4PpcuAy4BohC47WolMnk2IlUDyb1HnmBzmXzO965BBeg2rD1ZB570o2HcnWIxZBXxzkIJzwdmFZB3axLDSVBZCQRbZBzX9xMvi5TrzHkdSashtPlHZBKJ9kOHPw0SBx7wZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
