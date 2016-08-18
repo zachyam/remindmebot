49a8047f70d888466ef6b09a2d46671371d70c4f
zyam <thepredestrian@gmail.com>
2016-08-19 00:32:51 +0800

    test

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function (req, res) {
             text = event.message.text
 
             if (lastText === 'countdown') {
-              sendTextMessage(sender, "Sure! We will remind you in " + text + "minutes");  
+              sendTextMessage(sender, "Sure! We will remind you in " + text + " minutes");  
               continue;
             }
 
