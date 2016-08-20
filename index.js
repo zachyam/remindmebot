fea8bd63ca8df360002d12a36dc8101d51401f7e
zyam <thepredestrian@gmail.com>
2016-08-21 01:16:14 +0800

    test

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
+var lastText;
+
 app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging
-    var lastText;
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
