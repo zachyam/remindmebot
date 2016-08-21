66f33c75a9fbe1a6acfc01faf189bc05f70842dc
zyam <thepredestrian@gmail.com>
2016-08-21 22:04:29 +0800

    test

--- index.js
+++ index.js
@@ -59,16 +59,18 @@ function sendTextMessage(sender, text) {
 
 
 app.post('/webhook/', function (req, res) {
+    var lastText;
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
+            sendTextMessage(sender, text);
 
             if (text === "remindme") {
               sendTextMessage(sender, "What?");
-              var lastText = 'on';
+              lastText = 'on';
               continue;
             } 
 
