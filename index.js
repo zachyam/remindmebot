f9a7e64f0ebe700907d661deea572ed0dc308ef5
zyam <thepredestrian@gmail.com>
2016-08-24 01:28:26 +0800

    test

--- index.js
+++ index.js
@@ -62,10 +62,10 @@ var subject = null;
 app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
+        event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
         if (event.message && event.message.text) {
-            text = event.message.text
+            var text = event.message.text;
 
             sendTextMessage(sender, text);
             sendTextMessage(sender, i);
